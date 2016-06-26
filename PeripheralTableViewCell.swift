b35c2848005741bc43b9bffe14344eaad5128080
Eirik Vikanes <eirik.vikanes@gmail.com>
2016-06-26 11:40:55 +0200

    The app now ranges alle Eddystone beacons using their RSSI

---
+++ PeripheralTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PeripheralTableViewCell.swift
+//  beacon-position
+//
+//  Created by Eirik Vikanes on 26/06/16.
+//  Copyright © 2016 Eirik Vikanes. All rights reserved.
+//
+
+import UIKit
+
+class PeripheralTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var identifierTextField: UITextField!
+    @IBOutlet weak var rssiTextField: UITextField!
+}
